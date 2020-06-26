34dff749ed08f62306862c9f74f148073d1df04a
Gabriel Tioda Antonini <gabriel.t.antonini@gmail.com>
2020-06-26 13:10:12 -0300

    Completed

---
+++ desafio2/vetorobjeto.js
@@ -0,0 +1,8 @@
+const users = {
+    personaldata: [
+        {name: "Douglas", age: 24, technologies: "JavaScript, C++, Python", specialty: "Desktop"},
+        {name: "Diego", age: 22, technologies: ['Python', 'JavaScript', 'Java'], specialty: "Web/Mobile"}
+    ]
+}
+
+console.log(`The user ${users.personaldata[0].name} has ${users.personaldata[0].age} years old and his technologies is ${users.personaldata[0].technologies} with abbilities on ${users.personaldata[0].specialty}`)
