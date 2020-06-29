ea96d5116ae3184763fe0c473a67cffce3ebbbe7
Gabriel Tioda Antonini <gabriel.t.antonini@gmail.com>
2020-06-29 13:07:54 -0300

    fix

--- desafio2/vetorobjeto.js
+++ desafio2/vetorobjeto.js
@@ -1,7 +1,7 @@
 const users = {
     personaldata: [
         {name: "Douglas", age: 24, technologies: "JavaScript, C++, Python", specialty: "Desktop"},
-        {name: "Diego", age: 22, technologies: ['Python', 'JavaScript', 'Java'], specialty: "Web/Mobile"}
+        {name: "Diego", age: 22, technologies: "Python, JavaScript, Java", specialty: "Web/Mobile"}
     ]
 }
 
