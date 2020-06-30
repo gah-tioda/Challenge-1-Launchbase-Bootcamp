aac71cd194c22a46f19144b64c275010c4bd79f6
Gabriel Tioda Antonini <gabriel.t.antonini@gmail.com>
2020-06-30 14:03:49 -0300

    fix

--- desafio3/function.js
+++ desafio3/function.js
@@ -1,28 +1,29 @@
 const users = [
-    { name: "Gabriel", techs: ['HTML', 'CSS'] },
-    { name: "Doug", techs: ['JavaScript', 'CSS'] },
-    { name: "Maria", techs: ['HTML', 'Node.js'] }
-  ]
+  { name: "Gabriel", techs: ['HTML', 'CSS'] },
+  { name: "Doug", techs: ['JavaScript', 'CSS'] },
+  { name: "Maria", techs: ['HTML', 'Node.js'] }
+]
 
-  for (let user of users) {
-    console.log(`${user.name} works with ${user.techs.join(', ')}`)
+for (let user of users) {
+  console.log(`${user.name} works with ${user.techs.join(', ')}`)
 }
 
 
-  function checkCSS(user){
-    for (let tech of user.techs) {
-        if (tech == 'CSS') return true
-     } 
-     return false
+function checkCSS(user) {
+  for (let tech of user.techs) {
+    if (tech == 'CSS') return true
+  }
+  
+  return false
 }
 
 for (let i = 0; i < users.length; i++) {
   const userWorksWithCSS = checkCSS(users[i])
 
-  if(userWorksWithCSS) {
-      console.log(`The user ${users[i].name} works with CSS`)
+  if (userWorksWithCSS) {
+    console.log(`The user ${users[i].name} works with CSS`)
   }
 }
 
-  
-  
+
+
